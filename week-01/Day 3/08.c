35de00882341069a94e2f86bf0c214b01a5feedc
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-04 15:44:16 +0200

    08.c solved

---
+++ week-01/Day 3/08.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int h1 = 350;
+	int h2 = 200;
+	// tell if the double of h2 is bigger than h1
+	if (h2 * 2< h1);
+        printf("H2 is smaller than the double of H1");
+	return 0;
+}
