629a26a667b93caf077f1bb30bd896a32a1977aa
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-09 13:16:55 +0200

    practice MR

---
+++ week-02/Day 1/material rev practice.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void start_screen();
+int addition(int num1, int num2)
+int global_variable = 10;
+
+void start_screen()
+{
+    printf("hello user!\n");
+    printf("this is a program!\n");
+    printf("global_variable in main = %d\n", global_variable);
+    printf("have fun!\n");
+}
+
+
+int main()
+{
+    start_screen();
+    printf(("%d\n", addition(7, 3));
+
+    return 0;
+}
+
+int addition(int num1, int num2)
+{
+    int result = 0;
+    result = num1 + num2
+}
