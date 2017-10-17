ed8404f9a5179021953b3b2dbe457f4fd6bea1cd
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-17 15:23:54 +0200

    07.c copy array int to unsigned char

--- week-03/Day 2/07.c/main.c
+++ week-03/Day 2/07.c/main.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    int i_array[10] = {260, 259, 258, 257, 256, 255, 254, 253, 252, 251};
+        int i_array[10] = {260, 259, 258, 257, 256, 255, 254, 253, 252, 251};
     unsigned char uc_array[10];
-
+    //int a = 66;
 	//TODO: write a program, which copies the values from the given array to an unsigned char array
 	int i = 0;
 	for(i = 0; i < 10; i++){
-        i_array[i] = uc_array[i];
+        uc_array[i] = i_array[i];
         printf("Element %d is %d\n", i + 1, uc_array[i]);
 	}
     //print out the elements of the unsigned char array, with the "%d" format specifier
