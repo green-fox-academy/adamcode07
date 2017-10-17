2f4cf6a514467ef5ace96939fa41c596abedce10
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-17 15:02:22 +0200

    06.c copy & print values of array added

--- week-03/Day 2/06.c/main.c
+++ week-03/Day 2/06.c/main.c
@@ -5,11 +5,11 @@ int main()
 {
     float f_array[10] = {5.6, 8.73, 0.15, 0.99, 1.5, 6.3589, 10.01, -33.0, 0, 15};
     int i_array[10];
-    int i = 0;
 
 	//TODO: write a program, which copies the values from the given array to an integer array
 	  //memcpy(i_array, f_array, strlen(f_array)+1);
     //print out the elements of the integer array, with the "%d" format specifier
+    int i = 0;
     for(i = 0; i < 10; i++){
     i_array[i] = f_array[i];
     printf("Element %d is %d\n", i+1, i_array[i]);
---
+++ week-03/Day 2/07.c/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i_array[10] = {260, 259, 258, 257, 256, 255, 254, 253, 252, 251};
+    unsigned char uc_array[10];
+
+	//TODO: write a program, which copies the values from the given array to an unsigned char array
+	int i = 0;
+	for(i = 0; i < 10; i++){
+        i_array[i] = uc_array[i];
+        printf("Element %d is %d\n", i + 1, uc_array[i]);
+	}
+    //print out the elements of the unsigned char array, with the "%d" format specifier
+    //what do you see?
+
+
+    return 0;
+}
