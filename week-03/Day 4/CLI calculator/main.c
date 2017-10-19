23b0cb4f30a1ff977eaf4341e3f8848369f38df5
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-19 18:04:36 +0200

    calculator addition & subtrack

--- week-03/Day 4/CLI calculator/main.c
+++ week-03/Day 4/CLI calculator/main.c
@@ -2,26 +2,50 @@
 #include <string.h>
 #include <math.h>
 #include <windows.h>
+#include <stdlib.h>
 
 
 COORD coord = {0,0};
 void help();
 void set_cursor_pos(int x, int y);
+float summition();
+float subtraction();
 
 int main ()
 {
-    char command [256];
+    char command [100];
+    char* op_1;
+    char* op_2;
+    float input_1, input_2;
+    float result;
+
     help();
     set_cursor_pos(5, 5);
+    int x, y;
 
     while(strcmp(command, "exit") != 0){
+
         gets(command);
-        if(strcmp(command, "clear") == 0){
+    //    getchar();
+
+        if (strcmp(command, "clear") == 0){
             system("cls");
-            }
-        if(strcmp(command, "help") == 0){
+        }
+        if (strcmp(command, "help") == 0){
             help();
-            }
+        }
+
+        if  (strstr(command, "+") != 0){
+
+            summition(command);
+        }
+
+        if (strstr(command, "-") != 0){
+
+            subtraction(command);
+
+
+        }
     }
 
 	return 0;
@@ -63,3 +87,38 @@ void set_cursor_pos(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+float summition(char (command[]))
+{
+    char* op_1;
+    char* op_2;
+    float input_1, input_2;
+    float result = 0;
+
+    op_1 = strtok(command, "+");
+    op_2 = strtok(NULL, "+");
+
+    input_1 = atof(op_1);
+    input_2 = atof(op_2);
+
+    result = input_1 + input_2;
+
+    printf("%.2f", result);
+}
+
+float subtraction(char (command[]))
+{
+    char* op_1;
+    char* op_2;
+    float input_1, input_2;
+    float result;
+
+    op_1 = strtok(command, "-");
+    op_2 = strtok(NULL, "-");
+
+    input_1 = atof(op_1);
+    input_2 = atof(op_2);
+
+    result = input_1 - input_2;
+
+    printf("%2.f", result);
+}
