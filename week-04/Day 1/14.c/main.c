0fa68c8ba3c539d4fb499a77d942e67f64a7ba85
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-24 16:34:06 +0200

    14.c array cpy w while

---
+++ week-04/Day 1/14.c/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void string_copy(char *dest, char *source)
+{
+    int i = 0;
+    while(source[i] != '\0'){
+        dest[i] = source[i];
+        i++;
+    }
+    dest[i] = '\0';
+    printf("%s", dest);
+}
+
+int main()
+{
+    char my_string[] = "to be copied";
+    char dest[50];
+
+   //TODO: write a function, which copies a string to the 'dest' array, then print out in string format.
+    string_copy(dest, my_string);
+
+    return 0;
+}
