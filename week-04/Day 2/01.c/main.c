289ee2c7582cdf8f1f5569c1a778e03ad16616a6
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-25 11:19:46 +0200

    01.c change input & print

---
+++ week-04/Day 2/01.c/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+
+struct Computer {
+    float cpu_speed_GHz;
+    int ram_size_GB;
+    int bits;
+};
+
+int main() {
+    struct Computer computer1 = {3.2, 8, 32};
+
+    //TODO: Change the bits to 64
+//    struct Computer *struct_pointer;
+//    struct_pointer = &computer;
+    computer1.bits = 64;
+
+    //TODO: print out the structure members
+    //computer1.cpu_speed_GHz = 3.2;
+    //computer1.ram_size_GB = 8;
+    //computer1.bits = 32;
+
+    printf( "CPU speed : %f\n", computer1.cpu_speed_GHz);
+    printf( "CPU speed : %d\n", computer1.ram_size_GB);
+    printf( "CPU speed : %d\n", computer1.bits);
+
+    //printf( "CPu speed : %s\n", cpu_speed_GHz);
+//    printf( &computer );
+//    printf("cpu speed is %d\n", Computer{});
+//    printf("%d %d %d", sizeof(cpu_speed_GHz.computer), sizeof(ram_size_GB.computer), sizeof(bits.computer), sizeof(computer));
+//    Computer c1;
+//    c1 = (Computer){.id = id, .name = name, .score = score};
+//    shareedit
+
+
+    return 0;
+}
