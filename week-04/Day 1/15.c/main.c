2dd0d4a26f515daf9bd616aee8878c54f5d7a68a
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-26 14:22:39 +0200

    todo app start

--- week-04/Day 1/15.c/main.c
+++ week-04/Day 1/15.c/main.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
-void reverse_name(char *name, char *reversed_name);
-
-int main(void)
+void print_array(int *array, int size)
 {
-	char name[] = "Gipsz Jakab";
-
-    char reversed[50];
-
-    //write a function which reverse a name's last name and first name. Use a void function. Put your 'return' value in the second parameter.
-    reverse_name(name, reversed);
-
-
+    for(int i = 0; i < size; i++){
+        printf("a[%d] = %d\n", i, array[i]);
+    }
 }
 
-void reverse_name(char *name, char *reversed_name)
+
+int main()
 {
-    int i = 0;
-    while(name[i] != ' '){
-    reversed_name[i] = name[i];
-    }
-    reversed_name[i] = ' ';
-    printf("%s", reversed_name);
+	int a[] = {6, 8, 48, 1, -9, 89};
+    int size = sizeof(a)/sizeof(int);
+
+    print_array(a, size);
+	//TODO: write a function, which prints out the passed array's elements.
+
+	return 0;
 }
