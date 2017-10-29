ca4f1e8d360de3a881eb485553de7d5e024d5828
Adam Szalteleki <szaltelekia@gmail.com>
2017-10-29 20:13:42 +0100

    23 if-else/else if/else if-if/else

--- week-01/Day 3/23.c
+++ week-01/Day 3/23.c
@@ -1,21 +1,41 @@
-
 #include <stdio.h>
 #include <stdint.h>
-int main() {
-uint8_t ac = 8;
-uint8_t time = 120;
-uint8_t out;
 
-if ((ac%4 == 0) && (time <= 200)) {out = 0b100;}
-else if (time > 200) {out = 0b010;}
-else {out = 0b001;}
 
-printf("%d", out);
-// if ac is dividable by 4
-// and time is not more than 200
-// set out to 0b100
-// if time is more than 200
-// set out to 0b010
-// otherwise set out to 0b001
-return 0;
+int main()
+{
+	int boys_number = 10;
+	int girls_number = 0;
+
+	if (boys_number + girls_number > 20) {
+		if (boys_number == girls_number)
+			printf("Excellent party!");
+		else if (girls_number < 1)
+			printf("Sausage party.");
+		else
+			printf("Quite cool party.");
+	}
+	if (boys_number + girls_number < 20){
+        if (girls_number < 1)
+            printf("Sausage party.");
+        else
+            printf("Average party...");
+		}
+
+	return 0;
 }
+
+
+/* Write a program that has two int variables girls_number, and boys_number
+The first number represents the number of girls that comes to a party, the
+second the boys.
+It should print "The party is excellent!" if the the number of girls and
+boys are equal and there are more people coming than 20
+It should print "Quite cool party!"
+if there are more than 20 people coming but the girl - boy ratio is not 1-1
+It should print: "Average party..."
+if there are less people coming than 20
+It should print: "Sausage party"
+if no girls are coming, regardless the count of the people
+Run your program at least 3-4 times, change the variables to see the different outputs
+*/
