4f6f21dcd5f4866cf63ef2fbd327c297b1eee402
Adam Szalteleki <szaltelekia@gmail.com>
2017-11-12 00:18:38 +0100

    Console I/O 01.cpp donnez

---
+++ week-06/01.cpp/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+// Store their names in separate string type variables
+
+
+int main()
+{
+
+    string name1 = "Esther";
+    string name2 = "Mary";
+    string name3 = "Joe";
+
+
+    cout << "Hello, Esther!" << endl;
+    cout << endl << endl;
+    cout << "Hello, Mary!" << endl;
+    cout << "Hello, Joe!";
+
+    return 0;
+}
