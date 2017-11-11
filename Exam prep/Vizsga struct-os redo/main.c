9a250524cdb39021ca11afc59dff255eeab9f3cc
Adam Szalteleki <szaltelekia@gmail.com>
2017-11-11 12:20:26 +0100

    get oldest alive done with wrong result

--- Exam prep/Vizsga struct-os redo/main.c
+++ Exam prep/Vizsga struct-os redo/main.c
@@ -1,8 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    elementary_school,
+    high_school,
+    bsc_degree,
+    master_degree,
+    phd
+
+} qualification_t;
+
+struct person {
+
+    char name[10];
+    int birth_year;
+    qualification_t qualification;
+
+};
+
+int get_oldest_alive(struct person *p, int len);
+
+int get_qualification_count(struct person *p, int len, qualification_t qualification);
+
+
 int main()
 {
-    printf("Hello world!\n");
+    struct person Person[] = {
+
+        {"Adam", 1984, phd},
+        {"Gergo", 1982, bsc_degree},
+        {"Oszkar", 1990, high_school},
+        {"Sanyi", 1900, phd},
+
+    };
+
+    int len = sizeof(Person)/sizeof(Person[0]);
+    printf("%d\n", len);
+
+    printf("The oldest person is %d years old\n", get_oldest_alive(Person, len));
+    //printf("People with 'quali' qualification: %d\n", get_qualification_count(Person, len, quali));
+
     return 0;
+}
+
+int get_oldest_alive(struct person *p, int len)
+{
+    int current_year = 2017;
+    int oldest_max = 0;
+    int oldest_index = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if ((current_year - p[i].birth_year) > oldest_max)
+        {
+            oldest_max = p[i].birth_year;
+            oldest_index = i;
+        }
+    }
+    return (current_year - p[oldest_index].birth_year);
+}
+
+int get_qualification_count(struct person *p, int len, qualification_t qualification)
+{
+    int ppl_counter = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (p[i].qualification == quali)
+            ppl_counter++;
+    }
+
+    return ppl_counter;
 }
