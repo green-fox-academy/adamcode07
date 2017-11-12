0dd4e4788858a3ff9a5f951d90e0422b004db9d4
Adam Szalteleki <szaltelekia@gmail.com>
2017-11-12 20:32:13 +0100

    Datatype 02.cpp done in 4 mins

---
+++ week-06/Datatypes 02.cpp/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean
+
+int main()
+{
+
+    string name = "Adam Szalteleki";
+    int age = 32;
+    double height = 1.8;
+
+    cout << name << endl;
+    cout << age << endl;
+    cout << height << endl;
+
+    if(false)
+        cout << "Married" << endl;
+    else
+        cout << "Single" << endl;
+
+    return 0;
+}
