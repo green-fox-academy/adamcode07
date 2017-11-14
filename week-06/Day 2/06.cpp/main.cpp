69874341939f1c59bc96fd8abce864c6ecef58bd
Adam Szalteleki <szaltelekia@gmail.com>
2017-11-14 16:13:25 +0100

    06.cpp done

---
+++ week-06/Day 2/06.cpp/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Car {
+        private:
+            string type;
+            int km;
+
+        public:
+            Car (string type, int km){
+                this->type = type;
+                this->km = km;
+            }
+
+            int run(int num){
+                return (num + km);
+            }
+            string greet(){
+
+            return type;
+            }
+    };
+
+int main() {
+
+    Car cr ("Toyota", 25);
+    // Create a class called "Car"
+    // It should have a "type" property that stores the car's type in a string eg: "Mazda"
+    // It should have a "km" property that stores how many kilometers it have run
+    // The km and the type property should be a parameter in the constructor
+    // It should have a method called "run" that takes a number and increments the "km" property by it
+    cout << cr.greet() << cr.run(15) << endl;
+
+  return 0;
+}
