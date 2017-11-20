9e5074e06803feb8e654f696debadb87e24723d7
Adam Szalteleki <szaltelekia@gmail.com>
2017-11-20 22:02:30 +0100

    01.cpp added

--- week-07/Day 2/01.cpp/main.cpp
+++ week-07/Day 2/01.cpp/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+// create a function that adds two integers.
+// Create an other function of the same name that takes 3 integers and returns the result of adding these 3 integers.
+// Write code that uses both after one an other and prints out the results in a new line each.
+// For input use 1 and 2, then 1, 2 and 3.
+
+void sum_func(int a, int b);
+int sum_func(int a, int b, int c);
+
+int main() {
+
+
+	int sum = 0;
+
+	int x = 1;
+	int y = 2;
+	int z = 3;
+
+	sum_func(x, y);
+	cout << sum_func(x, y, z);
+
+	return 0;
+}
+
+void sum_func(int a, int b){
+    cout << a + b << endl;
+}
+
+int sum_func(int a, int b, int c){
+    return a+b+c;
 }
